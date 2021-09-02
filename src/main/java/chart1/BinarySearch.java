2a0efd2683eca88217abdc41df03bc88bf7aaec3
JoeyLi <591670166@qq.com>
2021-09-03 00:32:03 +0800

    feat(char1): add a new algoirthm to the project
    int 类型的二分查找法,递归实现

--- src/main/java/chart1/BinarySearch.java
+++ src/main/java/chart1/BinarySearch.java
@@ -13,4 +13,17 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public static int rankOnRecursion(int key, int[] a) {
+        return rankOnRecursion(key, a, 0, a.length - 1);
+    }
+
+    private static int rankOnRecursion(int key, int[] a, int lo, int hi) {
+        //如果key存在于a[]中， 它的索引不会小于lo且不会大于hi
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) return rankOnRecursion(key, a, lo, mid - 1);
+        else if (key > a[mid]) return rankOnRecursion(key, a, mid + 1, hi);
+        else return mid;
+    }
 }
